fafc078ad113fccbb177b909ba844fee251fbd5c
Teddymanguy <kjmork2@gmail.com>
2018-08-31 15:25:25 -0500

    Stuff

---
+++ RPGAssignment.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	int dif,choice = 0;
+	
+	printf("Welcome to my game\n");
+	printf("Please choose an option\n");
+	printf("1. Play\n2. Difficulty: %d\n3. Quit\n",dif);
+	scanf("%d", choice);
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
